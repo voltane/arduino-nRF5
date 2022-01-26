e63d9ac8aed6fe43bfed9bedbe2e2dfaa3ea60f8
Zbigniew Hadrian <zbigniew.hadrian@unitygroup.com>
2022-01-26 22:46:31 +0100

    Copied fix for low-power mode

--- cores/nRF5/WInterrupts.c
+++ cores/nRF5/WInterrupts.c
@@ -1,17 +1,14 @@
 /*
   Copyright (c) 2015 Arduino LLC.  All right reserved.
   Copyright (c) 2016 Sandeep Mistry All right reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -24,7 +21,7 @@
 
 #include <string.h>
 
-#if defined(NRF52_SERIES)
+#ifdef NRF52
 #define NUMBER_OF_GPIO_TE 8
 #else
 #define NUMBER_OF_GPIO_TE 4
@@ -32,6 +29,7 @@
 
 static voidFuncPtr callbacksInt[NUMBER_OF_GPIO_TE];
 static int8_t channelMap[NUMBER_OF_GPIO_TE];
+static voidFuncPtr callbackLowAccuracyInt;
 static int enabled = 0;
 
 /* Configure I/O interrupt sources */
@@ -39,10 +37,11 @@ static void __initialize()
 {
   memset(callbacksInt, 0, sizeof(callbacksInt));
   memset(channelMap, -1, sizeof(channelMap));
+  callbackLowAccuracyInt = NULL;
 
   NVIC_DisableIRQ(GPIOTE_IRQn);
   NVIC_ClearPendingIRQ(GPIOTE_IRQn);
-  NVIC_SetPriority(GPIOTE_IRQn, 2);
+  NVIC_SetPriority(GPIOTE_IRQn, 1);
   NVIC_EnableIRQ(GPIOTE_IRQn);
 }
 
@@ -125,23 +124,84 @@ void detachInterrupt(uint32_t pin)
   }
 }
 
+/*
+ * \brief Specifies a named Interrupt Service Routine (ISR) to call when an interrupt occurs.
+ *        Replaces any previous function that was attached to the interrupt.
+ *        Only one callback can be assigned to the low accuracy interrupt AKA PORT event.
+ */
+void attachInterruptLowAccuracy(uint32_t pin, voidFuncPtr callback, uint32_t mode)
+{
+  if (!enabled) {
+    __initialize();
+    enabled = 1;
+  }
+
+  if (pin >= PINS_COUNT) {
+    return;
+  }
+
+  pin = g_ADigitalPinMap[pin];
+
+  uint32_t polarity;
+
+  switch (mode) {
+    case FALLING:
+      NRF_GPIO->PIN_CNF[pin] |= ((uint32_t)GPIO_PIN_CNF_SENSE_Low << GPIO_PIN_CNF_SENSE_Pos);
+      break;
+
+    case RISING:
+      NRF_GPIO->PIN_CNF[pin] |= ((uint32_t)GPIO_PIN_CNF_SENSE_High << GPIO_PIN_CNF_SENSE_Pos);
+      break;
+
+    default:
+      return;
+  }
+ 
+  callbackLowAccuracyInt = callback;
+  NRF_GPIOTE->INTENSET = GPIOTE_INTENSET_PORT_Msk;
+}
+
+/*
+ * \brief Turns off the given low accuracy interrupt.
+ */
+void detachInterruptLowAccuracy(uint32_t pin)
+{
+  if (pin >= PINS_COUNT) {
+    return;
+  }
+
+  pin = g_ADigitalPinMap[pin];
+
+  callbackLowAccuracyInt = NULL;
+
+  NRF_GPIO->PIN_CNF[pin] &= ~((uint32_t)GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos);
+  NRF_GPIOTE->INTENCLR = GPIOTE_INTENCLR_PORT_Msk;
+}
+
 void GPIOTE_IRQHandler()
 {
-  uint32_t event = offsetof(NRF_GPIOTE_Type, EVENTS_IN[0]);
+  if(NRF_GPIOTE->EVENTS_PORT == 1 && (NRF_GPIOTE->INTENSET & GPIOTE_INTENSET_PORT_Msk)) {
+    if(callbackLowAccuracyInt) {
+      callbackLowAccuracyInt();
+    }
+    NRF_GPIOTE->EVENTS_PORT = 0;
+  } else {
+    uint32_t event = offsetof(NRF_GPIOTE_Type, EVENTS_IN[0]);
 
-  for (int ch = 0; ch < NUMBER_OF_GPIO_TE; ch++) {
-    if ((*(uint32_t *)((uint32_t)NRF_GPIOTE + event) == 0x1UL) && (NRF_GPIOTE->INTENSET & (1 << ch))) {
-      if (channelMap[ch] != -1 && callbacksInt[ch]) {
-        callbacksInt[ch]();
+    for (int ch = 0; ch < NUMBER_OF_GPIO_TE; ch++) {
+      if ((*(uint32_t *)((uint32_t)NRF_GPIOTE + event) == 0x1UL) && (NRF_GPIOTE->INTENSET & (1 << ch))) {
+        if (channelMap[ch] != -1 && callbacksInt[ch]) {
+          callbacksInt[ch]();
+        }
+
+      *(uint32_t *)((uint32_t)NRF_GPIOTE + event) = 0;
+#if __CORTEX_M == 0x04
+      volatile uint32_t dummy = *((volatile uint32_t *)((uint32_t)NRF_GPIOTE + event));
+      (void)dummy;
+#endif
       }
 
-    *(uint32_t *)((uint32_t)NRF_GPIOTE + event) = 0;
-#if __CORTEX_M == 0x04
-    volatile uint32_t dummy = *((volatile uint32_t *)((uint32_t)NRF_GPIOTE + event));
-    (void)dummy;
-#endif
+      event = (uint32_t)((uint32_t)event + 4);
     }
-
-    event = (uint32_t)((uint32_t)event + 4);
   }
 }
--- cores/nRF5/WInterrupts.h
+++ cores/nRF5/WInterrupts.h
@@ -1,16 +1,13 @@
 /*
   Copyright (c) 2015 Arduino LLC.  All right reserved.
-
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
-
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
   See the GNU Lesser General Public License for more details.
-
   You should have received a copy of the GNU Lesser General Public
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -46,6 +43,18 @@ void attachInterrupt(uint32_t pin, voidFuncPtr callback, uint32_t mode);
  * \brief Turns off the given interrupt.
  */
 void detachInterrupt(uint32_t pin);
+
+/*
+ * \brief Specifies a named Interrupt Service Routine (ISR) to call when an interrupt occurs.
+ *        Replaces any previous function that was attached to the interrupt.
+ *        Only one callback can be assigned to the low accuracy interrupt AKA PORT event.
+ */
+void attachInterruptLowAccuracy(uint32_t pin, voidFuncPtr callback, uint32_t mode);
+
+/*
+ * \brief Turns off the given low accuracy interrupt.
+ */
+void detachInterruptLowAccuracy(uint32_t pin);
 
 #ifdef __cplusplus
 }
